c7c8973ae6edce7c0efc30e1c4046d70ad7ddb56
Bhanavi Goyal <bhanavigoyal@gmail.com>
2024-02-22 09:39:16 +0530

    added pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
+import React from "react"
+import { BrowserRouter, createBrowserRouter,Route,RouterProvider, Routes } from "react-router-dom"
+import {SignUp} from "./pages/SignUp"
+import {SignIn} from "./pages/SignIn"
+import {Dashboard} from "./pages/Dashboard"
+import {SendMoney} from "./pages/SendMoney"
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/signin" element={<SignIn/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/send" element={<SendMoney/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
